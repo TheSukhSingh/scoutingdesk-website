4cb4592f8ae295411471e3401e616e46a17ff785
Sukhpreet Singh <sukhpreetsingh12323@gmail.com>
2026-04-27 23:08:28 +0530

    .

--- config/urls.py
+++ config/urls.py
@@ -27,11 +27,3 @@ urlpatterns += [
     path('accounts/google/login/', block_social),
     path('accounts/google/login/callback/', block_social),
 ]
-from core import dev_views as core_views
-
-urlpatterns += [
-    path('dev/reset/', core_views.dev_password_reset),
-    path('dev/reset/done/', core_views.dev_password_reset_done),
-    path('dev/reset/confirm/', core_views.dev_password_reset_confirm),
-    path('dev/reset/complete/', core_views.dev_password_reset_complete),
-]

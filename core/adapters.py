f43a977cf0ab337aaa3971b87b42a5ca0ad35b8f
Sukhpreet Singh <sukhpreetsingh12323@gmail.com>
2026-04-29 13:02:02 +0530

    .

--- core/adapters.py
+++ core/adapters.py
@@ -3,3 +3,9 @@ from allauth.account.adapter import DefaultAccountAdapter
 class CustomAccountAdapter(DefaultAccountAdapter):
     def populate_username(self, request, user):
         user.username = user.email
+
+    def is_open_for_signup(self, request):
+        return True
+    def is_open_for_signup(self, request):
+        print("🔥 SIGNUP CHECK HIT")
+        return True

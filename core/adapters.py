fdfee02e1e48263104d3654aca4986cb730d5c2d
Sukhpreet Singh <sukhpreetsingh12323@gmail.com>
2026-04-29 13:14:27 +0530

    .

--- core/adapters.py
+++ core/adapters.py
@@ -7,3 +7,9 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         print("🔥 SIGNUP CHECK HIT")
         return True
+    
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            return x_forwarded_for.split(',')[0]
+        return request.META.get('REMOTE_ADDR')

00cf42d6f487acd3aaa36715d341aa7fce47be23
Sukhpreet Singh <sukhpreetsingh12323@gmail.com>
2026-06-19 15:16:15 +0530

    mail using custom design

--- core/adapters.py
+++ core/adapters.py
@@ -1,18 +1,34 @@
 from allauth.account.adapter import DefaultAccountAdapter
 
-from activation.utils import get_client_ip
-from django.contrib.auth import get_user_model
-User = get_user_model()
+from core.emails import (
+    send_verification_email,
+    send_password_reset_email,
+)
+
 
 class CustomAccountAdapter(DefaultAccountAdapter):
+
     def populate_username(self, request, user):
         user.username = user.email
 
-    def is_open_for_signup(self, request):
-        return True
+    def send_mail(self, template_prefix, email, context):
 
-    def get_signup_redirect_url(self, request):
-        return "/#packages"
-    
-    def get_client_ip(self, request):
-        return get_client_ip(request)
+        if template_prefix == "account/email/email_confirmation":
+
+            send_verification_email(
+                user=context["user"],
+                activate_url=context["activate_url"],
+            )
+
+        elif template_prefix == "account/email/password_reset_key":
+
+            send_password_reset_email(
+                user=context["user"],
+                reset_url=(
+                    context.get("password_reset_url")
+                    or context.get("reset_url")
+                ),
+            )
+
+        else:
+            super().send_mail(template_prefix, email, context)

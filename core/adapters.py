193610b39adc718c3dd0b17bc324e3ec0fbe77f0
Sukhpreet Singh <sukhpreetsingh12323@gmail.com>
2026-06-19 15:27:32 +0530

    a

--- core/adapters.py
+++ core/adapters.py
@@ -1,4 +1,6 @@
 from allauth.account.adapter import DefaultAccountAdapter
+
+from activation.utils import get_client_ip
 from core.emails import (
     send_verification_email,
     send_password_reset_email,
@@ -16,31 +18,33 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     def get_signup_redirect_url(self, request):
         return "/#packages"
 
-    def render_mail(self, template_prefix, email, context, headers=None):
+    def get_client_ip(self, request):
+        return get_client_ip(request)
+    def send_mail(self, *args, **kwargs):
+        print("🔥 SEND_MAIL")
+        return super().send_mail(*args, **kwargs)
 
-        print("🔥", template_prefix)
+    def render_mail(self, *args, **kwargs):
+        print("🔥 RENDER_MAIL")
+        return super().render_mail(*args, **kwargs)
+    # def send_mail(self, template_prefix, email, context):
 
-        if template_prefix == "account/email/email_confirmation":
+    #     if template_prefix == "account/email/email_confirmation":
 
-            send_verification_email(
-                context["user"],
-                context["activate_url"]
-            )
+    #         send_verification_email(
+    #             user=context["user"],
+    #             activate_url=context["activate_url"],
+    #         )
 
-            return None
+    #     elif template_prefix == "account/email/password_reset_key":
 
-        elif template_prefix == "account/email/password_reset_key":
+    #         send_password_reset_email(
+    #             user=context["user"],
+    #             reset_url=(
+    #                 context.get("password_reset_url")
+    #                 or context.get("reset_url")
+    #             ),
+    #         )
 
-            send_password_reset_email(
-                context["user"],
-                context["password_reset_url"]
-            )
-
-            return None
-
-        return super().render_mail(
-            template_prefix,
-            email,
-            context,
-            headers=headers
-        )
+    #     else:
+    #         super().send_mail(template_prefix, email, context)
